722ba5205c362c79d91829f8c5cc81ba0b7f4d73
uli <uli@krispel.net>
2015-12-19 16:04:46 +0100

    day18

--- adventofcode2015/day18.js
+++ adventofcode2015/day18.js
@@ -1,9 +1,11 @@
 'use strict';
 // Like a GIF For Your Yard
+var fs=require('fs');
 
-var input = ".#.#.#\n...##.\n#....#\n..#...\n#.#..#\n####..".split("\n");
+//var input = ".#.#.#\n...##.\n#....#\n..#...\n#.#..#\n####..".split("\n");
+var input = fs.readFileSync('day18-input.txt', 'UTF8').split('\n');
 
-var SIZE = 6;
+var SIZE = 100;
 var LIGHTS = [];
 var currentline = 0;
 
@@ -63,10 +65,53 @@ function printLights(lights)
   }
 }
 
-input.forEach(parseNextLine);
-// console.log(lights);
+function writePBM(lights, index)
+{
+  var pbm = "P1\n# ulikk adventofcode2015 day18\n" + SIZE + " " + SIZE + "\n";
+  for(var y=0; y<SIZE; ++y){
+    var line = [];
+    for(var x=0; x<SIZE; ++x){
+      line.push(lights[y][x] ? '1' : '0');
+    }
+    pbm = pbm.concat(line.join(" ") + "\n");
+  }
+  fs.writeFileSync("lights"+index+".pbm", pbm);
+}
 
-printLights(LIGHTS);
-LIGHTS = processLights(LIGHTS);
-console.log('-');
-printLights(LIGHTS);
+var I=0;
+function doStep(l)
+{
+  var r = processLights(l);
+  r[0][0] = true;
+  r[99][0] = true;
+  r[99][99] = true;
+  r[0][99] = true;
+  writePBM(r,I++);
+  //printLights(r);
+  //console.log('-');
+  return r;
+}
+
+function countLights(lights)
+{
+  var count=0;
+  for(var y=0; y<SIZE; ++y){
+    for(var x=0; x<SIZE; ++x){
+      if (lights[y][x]) count++;
+    }
+  }
+  return count;
+}
+
+input.forEach(parseNextLine);
+  LIGHTS[0][0] = true;
+  LIGHTS[99][0] = true;
+  LIGHTS[99][99] = true;
+  LIGHTS[0][99] = true;
+// console.log(lights);
+//printLights(LIGHTS);
+for (var steps=0; steps<100; ++steps)
+{
+  LIGHTS = doStep(LIGHTS);
+}
+console.log(countLights(LIGHTS));
