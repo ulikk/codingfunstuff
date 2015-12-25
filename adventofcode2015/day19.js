93b3f841478ce1de3805f6353c1d492badd56b7d
ulikk <uli@krispel.net>
2015-12-25 14:38:09 +0100

    day19 first part

--- adventofcode2015/day19.js
+++ adventofcode2015/day19.js
@@ -3,10 +3,10 @@ var fs=require('fs');
 
 // Medicine for Rudolph
 
-//var RULES = {
+// var RULES = {
 //  "H" : [ "HO", "OH" ],
 //  "O" : [ "HH" ]
-//};
+// };
 var RULES={};
 var input = fs.readFileSync('day19-input.txt', 'UTF8').split('\n');
 input.forEach(function(line){
@@ -32,29 +32,52 @@ function allVariations(ruleset, molecule)
   var tokens=[];
   var total = 0;
   var molecules = {};
+  var POS={};
   // get tokens
   for (var atom in ruleset)
   {
+    POS[atom]=[];
     var rpos = getAllPos(molecule, atom);
-    rpos.forEach(function(i){ tokens[i]=atom;});
+    rpos.forEach(function(i){ 
+      //tokens[i]=atom;
+      POS[atom].push(i);
+    });
     total += rpos.length * ruleset[atom].length;
     //console.log(atom + ":" + rpos);
   }
-  console.log("tokens:" + tokens);
+  //console.log("tokens:" + tokens);
+  console.log(POS);
   console.log("total replacements: " + total);
 
-  for (var i = 0; i<tokens.length; ++i) 
+  // perform replacement
+  for (var atom in POS)
   {
-    // test all variations at this position
-    for (var j = 0; j<ruleset[tokens[i]].length; ++j)
+    for (var i=0; i<POS[atom].length; ++i)
     {
-       var t = tokens.slice();
-       t[i] = ruleset[tokens[i]][j];  // replace
-       var molecule = t.join("");
-       //console.log("molecule: " + molecule);
-       molecules[molecule] = true;
+      var p=POS[atom][i];
+      for (var r=0; r<ruleset[atom].length;++r)
+      {
+        var repl = ruleset[atom][r];
+        var R = molecule.substring(0, p) + repl + molecule.substring(p+atom.length); 
+        //console.log(R);
+        molecules[R] = true;
+      }
     }
   }
+
+  // for (var i = 0; i<tokens.length; ++i) 
+  // {
+  //   // test all variations at this position
+  //   for (var j = 0; j<ruleset[tokens[i]].length; ++j)
+  //   {
+  //      var t = tokens.slice();
+  //      t[i] = ruleset[tokens[i]][j];  // replace
+  //      var molecule = t.join("");
+  //      //console.log("molecule: " + molecule);
+  //      molecules[molecule] = true;
+  //   }
+  // }
+  console.log(POS);
   return molecules;
 }
 
